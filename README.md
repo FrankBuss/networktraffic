dc6d5bebff50ba045b29ba606592c0c2d9052aed
Frank Buss <fb@frank-buss.de>
2021-12-11 14:27:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # networktraffic
-simple network traffic logger
+
+This is a simple network traffic logger, which logs the network traffic of one network device, and prints summary information for the last 30 days.
+
+# how to compile and run the program
+
+First install Rust, e.g. from https://rustup.rs. You can build the program with cargo build --release.
+
+For creating the log, start it with the network interface and log it to a file:
+
+```
+./release/networktraffic -i eth0 > log.txt
+```
+
+This creates a log file, with the number of bytes sent and received for each minute, like this:
+
+```
+1639228696 0 0
+1639228756 51083788 1276943
+1639228816 50860637 1717584
+1639228876 50256986 1263405
+1639228936 52473745 1663039
+1639228996 47932548 1089242
+1639229056 39167617 1050084
+1639229116 43437693 1076336
+```
+
+To show the network traffic for the last 30 days, call it with the log filename as an argument:
+
+```
+./release/networktraffic -f log.txt
+```
+
+Example output:
+```
+day: 1, sent: 0 B, received: 0 B
+day: 2, sent: 0 B, received: 0 B
+day: 3, sent: 0 B, received: 0 B
+day: 4, sent: 0 B, received: 0 B
+day: 5, sent: 0 B, received: 0 B
+day: 6, sent: 0 B, received: 0 B
+day: 7, sent: 0 B, received: 0 B
+day: 8, sent: 0 B, received: 0 B
+day: 9, sent: 0 B, received: 0 B
+day: 10, sent: 0 B, received: 0 B
+day: 11, sent: 0 B, received: 0 B
+day: 12, sent: 0 B, received: 0 B
+day: 13, sent: 0 B, received: 0 B
+day: 14, sent: 0 B, received: 0 B
+day: 15, sent: 0 B, received: 0 B
+day: 16, sent: 0 B, received: 0 B
+day: 17, sent: 0 B, received: 0 B
+day: 18, sent: 0 B, received: 0 B
+day: 19, sent: 0 B, received: 0 B
+day: 20, sent: 0 B, received: 0 B
+day: 21, sent: 0 B, received: 0 B
+day: 22, sent: 0 B, received: 0 B
+day: 23, sent: 0 B, received: 0 B
+day: 24, sent: 0 B, received: 0 B
+day: 25, sent: 0 B, received: 0 B
+day: 26, sent: 0 B, received: 0 B
+day: 27, sent: 0 B, received: 0 B
+day: 28, sent: 0 B, received: 0 B
+day: 29, sent: 0 B, received: 0 B
+day: 30, sent: 319.7 MB, received: 8.7 MB
+sum: sent: 319.7 MB, received: 8.7 MB
+```
---
+++ src/main.rs
@@ -0,0 +1,164 @@
+use clap::{App, Arg};
+use human_bytes::human_bytes;
+use std::fs::File;
+use std::io::stdout;
+use std::io::{self, BufRead, Write};
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::{thread, time};
+
+#[derive(Clone, Copy, Debug)]
+struct NetworkStatus {
+    bytes_sent: u64,
+    bytes_received: u64,
+}
+
+impl NetworkStatus {
+    fn new() -> NetworkStatus {
+        NetworkStatus {
+            bytes_sent: 0,
+            bytes_received: 0,
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn get_network_status(interface: &str) -> NetworkStatus {
+    let mut status = NetworkStatus::new();
+    let mut found = false;
+
+    if let Ok(lines) = read_lines("/proc/net/dev") {
+        for line in lines {
+            if let Ok(line) = line {
+                let fields = line
+                    .split(" ")
+                    .filter(|&x| !x.is_empty())
+                    .collect::<Vec<&str>>();
+                if fields.len() < 12 {
+                    continue;
+                }
+                if fields[0] != (interface.to_string() + &":".to_string()) {
+                    continue;
+                }
+                status.bytes_received = fields[1].parse().unwrap();
+                status.bytes_sent = fields[9].parse().unwrap();
+                found = true;
+                break;
+            }
+        }
+    }
+    if !found {
+        println!("network interface {} not found", interface);
+        std::process::exit(1);
+    }
+
+    status
+}
+
+fn get_unix_timestamp() -> u64 {
+    let now = SystemTime::now();
+    now.duration_since(UNIX_EPOCH).unwrap().as_secs()
+}
+
+fn evalutate_log(file: &str) {
+    const DAYS: usize = 30;
+    let mut days = vec![NetworkStatus::new(); DAYS];
+    let secs_per_day = 3600 * 24;
+    let now = get_unix_timestamp();
+    let start = now - (secs_per_day * DAYS) as u64;
+    if let Ok(lines) = read_lines(file) {
+        for line in lines {
+            if let Ok(line) = line {
+                let fields = line.split(" ").collect::<Vec<&str>>();
+                if fields.len() < 3 {
+                    continue;
+                }
+                let timestamp: u64 = fields[0].parse().unwrap();
+                if timestamp < start {
+                    continue;
+                }
+                let sent: u64 = fields[1].parse().unwrap();
+                let received: u64 = fields[2].parse().unwrap();
+                let day = (timestamp - start) / (secs_per_day as u64);
+                if day < days.len() as u64 {
+                    days[day as usize].bytes_sent += sent;
+                    days[day as usize].bytes_received += received;
+                }
+            }
+        }
+    }
+    let mut sum_sent: u64 = 0;
+    let mut sum_received: u64 = 0;
+    for (i, day) in days.iter().enumerate() {
+        println!(
+            "day: {}, sent: {}, received: {}",
+            i + 1,
+            human_bytes(day.bytes_sent as f64),
+            human_bytes(day.bytes_received as f64)
+        );
+        sum_sent += day.bytes_sent;
+        sum_received += day.bytes_received;
+    }
+    println!(
+        "sum: sent: {}, received: {}",
+        human_bytes(sum_sent as f64),
+        human_bytes(sum_received as f64)
+    );
+}
+
+fn create_log(interface: &str) {
+    // log network traffic every minute
+    println!("{} 0 0", get_unix_timestamp());
+    stdout().flush().unwrap();
+    let mut last_status = get_network_status(interface);
+    loop {
+        thread::sleep(time::Duration::from_secs(60));
+        let status = get_network_status(interface);
+        let sent = status.bytes_sent - last_status.bytes_sent;
+        let received = status.bytes_received - last_status.bytes_received;
+        println!("{} {} {}", get_unix_timestamp(), sent, received);
+        stdout().flush().unwrap();
+        last_status = status;
+    }
+}
+
+fn main() {
+    let matches = App::new("networktraffic")
+        .version("0.1")
+        .author("Frank Buss <fb@frank-buss.de>")
+        .about("Logs network traffic and prints summary information")
+        .arg(
+            Arg::with_name("file")
+                .conflicts_with("interface")
+                .short("f")
+                .long("file")
+                .help("Config file to read, for showing summary information")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("interface")
+                .conflicts_with("file")
+                .short("i")
+                .long("interface")
+                .help("Network interface name, for example eth0")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let file = matches.value_of("file").unwrap_or("");
+    let interface = matches.value_of("interface").unwrap_or("");
+    if file != "" {
+        evalutate_log(file);
+    } else if interface != "" {
+        create_log(interface);
+    } else {
+        matches.usage();
+    }
+}
